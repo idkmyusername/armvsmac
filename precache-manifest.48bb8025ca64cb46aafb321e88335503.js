b6966b02a4738cecc4826e42e6d6354d7d91a30f
Сатурн 24 <s24_host3@MacBook-Pro-Saturn.local>
2020-06-10 12:48:03 +0300

    Updates

---
+++ precache-manifest.48bb8025ca64cb46aafb321e88335503.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e57c83634b496d482dbf840aaebdb98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce7f675578c644176ed",
+    "url": "/static/css/2.4911f66b.chunk.css"
+  },
+  {
+    "revision": "cf77adf512cddb94d9db",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "5ce7f675578c644176ed",
+    "url": "/static/js/2.31903af7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31903af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf77adf512cddb94d9db",
+    "url": "/static/js/main.42b6a14b.chunk.js"
+  },
+  {
+    "revision": "c803713d7fef9788b6d9",
+    "url": "/static/js/runtime-main.f73ffe82.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
